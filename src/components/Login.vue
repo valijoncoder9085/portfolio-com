48ed0e9c77edb54eb38224492cb3d4b38bc49983
Valijon Coder <valijoncoder9085@gmail.com>
2023-08-19 01:48:10 -0700

    commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -18,24 +18,20 @@
           <input class="form-input" type="password">
         </div>
         <div class="login--form__card">
-          <button class="card---form__btn">
+          <button class="card---form__btn rounded-10">
             Create Account
           </button>
           <p> Already have an account? <a href="#" style="color: olivedrab">Log in</a></p>
-          <p>or</p>
-          <div class="d-flex justify-space-between w-60 align-item-center">
-            <div>
-              <button class="d-flex align-item-center">
+          <p class="login--form__card--or">or</p>
+          <div class="button__card d-flex justify-content-center align-item-center">
+              <button class="d-flex align-item-center rounded-10">
                 <img src="../assets/images/google.svg" alt="Google Image">
                 Sing up with Google
               </button>
-            </div>
-            <div>
-              <button class="d-flex align-item-center">
+              <button class="d-flex align-item-center rounded-10">
                 <img width="50px" src="../assets/images/gitHub.svg" alt="GitHub Image">
                 Sing up with GitHub
               </button>
-            </div>
           </div>
         </div>
       </form>
@@ -45,7 +41,6 @@
 
 <script>
 export default {
-
   data() {
     return {
 
@@ -76,11 +71,12 @@ export default {
     position: absolute;
     top: 0;
     right: 0;
-    height: 100vh;
+    min-height: 100vh;
     width: 50%;
     display: flex;
-    justify-content: center;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
     .login--form {
       display: flex;
@@ -88,12 +84,14 @@ export default {
       flex-direction: column;
       align-self: center;
       width: 100%;
+
       &__item{
         display: flex;
         flex-direction: column;
         align-items: start;
         width: 70%;
         margin: 10px auto;
+
         .form-input{
           padding: 15px 8px;
           background: #EFF0F2;
@@ -107,16 +105,102 @@ export default {
           background: #007074;
           padding: 10px;
           margin: 10px 0;
+          width: 50%;
           color: #ffffff;
           font-size: 26px;
           border: 1.4px solid #D2D2D2;
         }
+        &--or{
+          color: #043133;
+          font-size: 26px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: normal;
+          text-transform: uppercase;
+          margin: 15px 0;
+        }
         .card---form__btn:hover{
           cursor: pointer;
         }
+        button{
+          margin: 0 10px;
+        }
       }
     }
-
   }
 }
+@media (max-width: 600px) {
+  .login{
+    &--card{
+      display: none;
+    }
+    &--input{
+      width: 100%;
+      background-image: url("../assets/images/image.svg");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      h1{
+        color: #d0dcdc;
+      }
+      .login--form {
+        color: white;
+
+        &__card{
+
+          .card---form__btn{
+            font-size: 14px;
+          }
+
+          .card---form__btn:hover{
+            cursor: pointer;
+          }
+
+          button{
+            img{
+              width: 22px;
+              margin-right: 5px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+@media (max-width: 900px) {
+  .button__card{
+    display: flex;
+    flex-direction: column;
+    button{
+      margin: 5px 0 !important;
+    }
+  }
+}
+@media (max-width: 1200px) {
+  .login--form__card{
+
+    .card---form__btn{
+      font-size: 22px !important;
+      width: 100% !important;
+    }
+  }
+  .login--form__card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 70%;
+    margin: 0 auto;
+    .button__card{
+      display: flex;
+    }
+    button{
+      width: 100%;
+      img{
+        width: 30px;
+      }
+    }
+  }
+}
+
+
 </style>
